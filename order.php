f44ea3e4aa3e899921a7ff3ec2d7dc118a531cd0
LENELJ <20102688@usc.edu.ph>
2021-12-02 17:33:36 +0800

    final commit

--- order.php
+++ order.php
@@ -15,16 +15,28 @@ if(isset($_POST['order_item'])){
 
         $insert= "INSERT INTO `workorder` (`order_id`,`client_id`,`order_date`) 
                   VALUES ('$uniq','$id','$date')";
-        $order="SELECT `order_id` 
+
+        if (mysqli_query($conn, $insert)) {
+            $order="SELECT `order_id` 
                 FROM `workorder`, `tattoo` 
                 WHERE `tattoo`.`tattoo_id`='$id' 
                 AND `workorder`.`client_id`='client id'
                 AND `workorder`.`order_date`=current date LIMIT 1";
-        $query= mysqli_query($conn,$order);
-        $row = mysqli_fetch_assoc($query);
-        $order_id = $row['order_id'];
+
+            $query= mysqli_query($conn,$order);
+            $row = mysqli_fetch_assoc($query);
+            $order_id = $row['order_id'];
+
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
     }
-    // insert into order_item
+        // insert into order_item
+    $unID=uniqid();
+    $ins_order= "INSERT INTO `order_item`(`item_id`,`order_id`,`tattoo_id`) 
+                VALUES ('$unID','$order_id','$client_id')";
+                
+    mysqli_query($conn,$ins_order);
     
 }
 
